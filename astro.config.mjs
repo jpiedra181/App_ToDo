f040e010bd54886cb43b2b2e52449a6ce8167887
JPiedra <javierpiedraiglesias@gmail.com>
2025-03-19 11:06:01 +0100

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+    site: 'https://jpiedra181.github.io',
+    base: 'deploy_test',
+});
