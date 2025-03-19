a3305c4ea242a09aa935874abc8520c7c9fd0853
JPiedra <javierpiedraiglesias@gmail.com>
2025-03-19 11:52:19 +0100

    update src icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://jpiedra181.github.io/',
-  base: '/deploy_test/',
+  base: '/deploy_test',
 
   vite: {
     plugins: [tailwindcss()],
