3a9a73e19fee90a59face0e890d008e0a6ac815b
JPiedra <javierpiedraiglesias@gmail.com>
2025-03-19 11:11:07 +0100

    add tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://jpiedra181.github.io',
-    base: 'deploy_test',
+  site: 'https://jpiedra181.github.io',
+  base: 'deploy_test',
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
