a06d56e0d2d7012af3d444ded768e3f4a9057966
JPiedra <javierpiedraiglesias@gmail.com>
2025-03-19 14:21:55 +0100

    rename repository

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://jpiedra181.github.io',
-  base: '/deploy_test',
+  base: '/App_ToDo',
 
   vite: {
     plugins: [tailwindcss()],
